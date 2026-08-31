cafe9529e8cdda42d77098d0cc08bfe08eb7785c
Jison <laureanoisonponce@gmail.com>
2026-08-31 16:13:42 -0300

    Agrego JavaScript inicial y evento de reserva

---
+++ Sintetico-Sporting/js/index.js
@@ -0,0 +1,5 @@
+console.log("JavaScript conectado correctamente");
+const botonReservar = document.getElementById("btnReservar");
+botonReservar.addEventListener("click", function () {
+    console.log("Hiciste clic en RESERVAR AHORA");
+    });
