209ab17ea00bb1d87374bfac5fc0474e6ccd8c52
Jison <laureanoisonponce@gmail.com>
2026-09-02 18:01:36 -0300

    Avanzo login frontend

--- Sintetico-Sporting/js/login.js
+++ Sintetico-Sporting/js/login.js
@@ -1,1 +1,24 @@
-dsdsds
+const formulario = document.getElementById("formLogin");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const mensajeError = document.getElementById("mensajeError");
+
+formulario.addEventListener("submit", function (event) {
+
+    event.preventDefault();
+
+    if (email.value === "" || password.value === "") {
+
+        mensajeError.textContent = "Completá todos los campos";
+
+    } else if (!email.value.includes("@")) {
+
+        mensajeError.textContent = "Ingresá un email válido";
+
+    } else {
+
+        mensajeError.textContent = "Datos completos";
+
+    }
+
+});
