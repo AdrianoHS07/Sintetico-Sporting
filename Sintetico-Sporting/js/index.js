1b67c31a9328eaf3bed1a5b027bde5bc0dcfef43
Jison <laureanoisonponce@gmail.com>
2026-09-02 16:17:08 -0300

    cambios

--- Sintetico-Sporting/js/index.js
+++ Sintetico-Sporting/js/index.js
@@ -3,3 +3,4 @@ const botonReservar = document.getElementById("btnReservar");
 botonReservar.addEventListener("click", function () {
     console.log("Hiciste clic en RESERVAR AHORA");
     });
+    
