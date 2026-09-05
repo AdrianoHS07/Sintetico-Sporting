ea608fc732973f759c532b6d1e7aab2b126fcc5e
Jison <laureanoisonponce@gmail.com>
2026-09-05 16:52:32 -0300

    Avanzo login y actualizo backend

--- backend/src/main/java/com/sinteticosporting/backend/controller/UsuarioController.java
+++ backend/src/main/java/com/sinteticosporting/backend/controller/UsuarioController.java
@@ -1,5 +1,36 @@
 package com.sinteticosporting.backend.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sinteticosporting.backend.entity.Usuario;
+import com.sinteticosporting.backend.service.UsuarioService;
+import com.sinteticosporting.backend.dto.LoginRequest;
+import com.sinteticosporting.backend.dto.LoginResponse;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
-    
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping("/registro")
+    public Usuario registrar(@RequestBody Usuario usuario) {
+
+        return usuarioService.registrarUsuario(usuario);
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody LoginRequest loginRequest) {
+
+        return usuarioService.login(
+            loginRequest.getEmail(),
+            loginRequest.getContraseña()
+        );
+    }
 }
